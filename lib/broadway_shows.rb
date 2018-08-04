adb48ac542eaa6528a0791a06ff284a7502fdd13
Richard Cho <richardcho100@gmail.com>
2018-08-04 21:35:43 +0000

    Require gems & files

--- lib/broadway_shows.rb
+++ lib/broadway_shows.rb
@@ -1,4 +1,10 @@
-require "broadway_shows/version"
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
+require_relative "broadway_shows/version"
+require_relative "broadway_shows/cli"
+require_relative "broadway_shows/show"
 
 module BroadwayShows
   # Your code goes here...
