12138df8f6248184dcecd018fd9dc59ec34f2e13
Richard Cho <richardcho100@gmail.com>
2018-08-23 00:08:47 +0000

    Add Scraper class w/methods to scrape whole doc & shows

---
+++ lib/broadway_shows/scraper.rb
@@ -0,0 +1,10 @@
+class BroadwayShows::Scraper
+
+  def self.get_page
+    Nokogiri::HTML(open("https://www.broadway.org/shows/index/currently_playing,-1,0,"))
+  end
+
+  def self.scrape_shows_index
+    self.get_page.css(".shows-list-item-info")
+  end
+end
